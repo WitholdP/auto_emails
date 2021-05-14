9a001884c14b2a5042805b06d9aa40c49d918538
Witold Piecewicz <w.piecewicz@gmail.com>
2021-05-14 11:28:41 +0200

    static files change in settings

--- auto_emails/settings/base.py
+++ auto_emails/settings/base.py
@@ -135,9 +135,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+
 
 
 MEDIA_URL = "/media/"
